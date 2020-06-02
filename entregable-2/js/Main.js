be82b338679b2d18d6e51f9a2593cc2a24b54d4d
mrivasalfano <mrivasalfano@gmail.com>
2020-06-02 17:35:19 -0300

    fix

--- entregable-2/js/Main.js
+++ entregable-2/js/Main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', e => {
     canvas.width = 1366;
     canvas.height = 600;
 
-    if (window.innerWidth >= 1920) {
+    if (window.innerWidth >= 1800) {
         document.body.style.padding = "100px";
         document.body.style.background = "rgb(120, 120, 120)";
     }
