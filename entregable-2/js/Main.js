23956d6bb3bdea5e6bffc24ecbb71b465f83ac31
mrivasalfano <mrivasalfano@gmail.com>
2020-06-02 17:50:19 -0300

    prueba auto centrado

--- entregable-2/js/Main.js
+++ entregable-2/js/Main.js
@@ -1,15 +1,7 @@
 document.addEventListener('DOMContentLoaded', e => {
     let canvas = document.querySelector('#gameBoard');
     let game;
-
-    canvas.width = 1366;
-    canvas.height = 600;
-
-    if (window.innerWidth >= 1800) {
-        document.body.style.padding = "100px";
-        document.body.style.background = "rgb(120, 120, 120)";
-    }
-
+    
     startGame();    
     
     function startGame() {
