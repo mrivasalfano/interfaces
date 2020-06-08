87acc1a4026c2d90009bfc0cff30ac3bf5c473ab
mrivasalfano <mrivasalfano@gmail.com>
2020-06-08 18:57:18 -0300

    acomode el ej del reloj

--- practico-3/ej4/version con CSS/js/main.js
+++ practico-3/ej4/version con CSS/js/main.js
@@ -1,16 +1,21 @@
 "use strict"
 
-document.addEventListener('DOMContentLoaded', e => {
+window.onload = e => {
     let secondsHand = document.querySelector('.seconds');
     let hoursHand = document.querySelector('.hours');
     let minutesHand = document.querySelector('.minutes');
 
     let date = new Date();
     let hours = date.getHours();
-    let minutes = date.getMinutes() * 6;
+    let minutes = date.getMinutes() * (360 / 60);
     let seconds = date.getSeconds() * 6;
     
-    secondsHand.style.rotate = (seconds) +  'deg';
-    minutesHand.style.rotate = (minutes) + 'deg';
-    hoursHand.style.rotate = (hours) + 'deg';
-});
+    if (hours > 12)
+        hours -=12;
+
+    hours = (hours * (360 / 12)) + (minutes/12);
+        
+    secondsHand.style.rotate = seconds +  'deg';
+    minutesHand.style.rotate = minutes + 'deg';
+    hoursHand.style.rotate = hours  + 'deg';
+};
--- practico-3/ej4/version con JS/js/main.js
+++ practico-3/ej4/version con JS/js/main.js
@@ -1,19 +1,40 @@
 "use strict"
 
-document.addEventListener('DOMContentLoaded', e => {
+window.onload = e => {
     let secondsHand = document.querySelector('.seconds');
     let hoursHand = document.querySelector('.hours');
     let minutesHand = document.querySelector('.minutes');
 
     let date = new Date();
+    
+    //pido la hora, minutos y segundos
     let hours = date.getHours();
-    let minutes = date.getMinutes() * 6;
-    let seconds = date.getSeconds() * 6;
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
     
-    
+    //si se pasa de las 12, por ej 18hs
+    if (hours > 12)
+        hours -= 12;
+
+    seconds = seconds * 6;
+    minutes = minutes * (360 / 60);
+    hours = (hours * (360/12)) + (minutes/12);
+
+    rotateHands();
+
+    //faltaría hacer if o algo para que no
+    //se pase de 360°
     setInterval(() => {
-        secondsHand.style.rotate = (seconds+=6) +  'deg';
-        minutesHand.style.rotate = (minutes+=0.1) + 'deg';
-        hoursHand.style.rotate = (hours+=0.0016) + 'deg';
+        rotateHands();
     }, 1000);
-});
+
+    function rotateHands() {
+        //a los segundos le sumo 6 grados porque
+        //360° / 60s = 6
+        secondsHand.style.rotate = (seconds+=6) +  'deg';
+        //360° / 60s * 60s = 0.1
+        minutesHand.style.rotate = (minutes+=0.1) + 'deg';
+        //360° / 60s * 60s * 12h = 0.00833333...
+        hoursHand.style.rotate = (hours+=0.0083) + 'deg';
+    }
+};
