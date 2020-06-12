8400eb163c661ec946c77123c3be58c85da0cb2e
mrivasalfano <mrivasalfano@gmail.com>
2020-06-12 16:07:52 -0300

    limpie codigo para seguir de la forma correcta

---
+++ entregable-3/js/Main.js
@@ -0,0 +1,20 @@
+window.onload = e => {
+    const playBtn = document.querySelector('#playGame');
+    
+    //botón para iniciar juego
+    playBtn.addEventListener('click', e => {
+        //oculto las reglas y las paso a un zindex atrás
+        //así no estorban
+        let rules = document.querySelector('#rules');
+        rules.style.display = 'none';
+        rules.style.zIndex = '-1';
+
+        //hago lo mismo que con las reglas pero con el 
+        //botón de iniciar
+        document.querySelector('#plane').classList.remove('hide');
+        playBtn.style.display = 'none';
+        playBtn.style.zIndex = '-1';
+        
+        game = new Game();
+    });
+};
--- entregable-3/js/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = e => {
-    const container = document.querySelector('#container');
-    let cont = 20;
-
-    window.addEventListener('click', e => {
-        container.style.bottom = cont + 'px';
-        cont+=30;
-    });
-};
