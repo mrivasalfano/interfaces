677390a6682bf39421dc0964ad6f76774da235b1
mrivasalfano <mrivasalfano@gmail.com>
2020-07-14 18:16:52 -0300

    prueba template por js

--- entregable-5/js/homepage.js
+++ entregable-5/js/homepage.js
@@ -1,5 +1,15 @@
 "use strict"
 
-document.addEventListener('DOMContentLoaded', e=> {
-    $('#prueba').multislider();
+document.addEventListener('DOMContentLoaded', e => {
+    document.querySelector('#btnBuscar').addEventListener('click', e=> {
+        location.replace('busqueda.html')
+    });
+
+    crearCards();
 });
+
+async function crearCards() {
+    let cardMusica = await fetch('../templates/card-musica.html');
+
+    document.querySelector('.main-container').innerHTML = cardMusica;
+}
