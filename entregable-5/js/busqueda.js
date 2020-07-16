35c4d556ca75dc73258d21948443a085c2df8622
mrivasalfano <mrivasalfano@gmail.com>
2020-07-16 16:22:03 -0300

    secciones de busqueda

--- entregable-5/js/busqueda.js
+++ entregable-5/js/busqueda.js
@@ -3,4 +3,19 @@
 document.addEventListener('DOMContentLoaded', e => {
     crearNavBar();
     crearReproductor();
+    crearSecciones();
 });
+
+async function crearSecciones() {
+    let titles = ['Canciones', 'Artistas', 'Álbums', 'Playlists', 'Podcasts'];
+    let seccionBusqueda = await fetch('templates/seccionBusqueda.html');
+    seccionBusqueda = await seccionBusqueda.text();
+
+    titles.forEach(title => {
+        document.querySelector('.main-container').innerHTML += seccionBusqueda;
+        let titulos = document.querySelectorAll('.seccionBusqueda__titulo');
+        titulos[titulos.length-1].innerHTML = title;
+    });
+
+    return true;
+}
