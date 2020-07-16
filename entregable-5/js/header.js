7fb3312a773d43ad379ca82e5c4dd8cadbf8f0b5
mrivasalfano <mrivasalfano@gmail.com>
2020-07-16 11:27:22 -0300

    reproducir cancio en el home

--- entregable-5/js/header.js
+++ entregable-5/js/header.js
@@ -1,17 +1,22 @@
 "use strict"
 
 async function crearNavBar() {
+    //pido el html del navbar
     let navBar = await fetch('templates/navbar.html');
     navBar = await navBar.text();
 
+    //se lo meto al header
     document.querySelector('header').innerHTML = navBar;
 
     document.querySelector('#btnBuscar').addEventListener('click', e => {
         location.replace('busqueda.html')
     });
     
-    let iconoHamburguesa = document.querySelector('#iconoHamburguesa');
+    //cuando se hace click en la hamburguesa agrego una clase
+    //a los links para que aparezcan o desaparezcan
+    let iconoHamburguesa = document.querySelector('#hamburguesa');
     let linksNavBar = document.querySelector('#links');
+    let body = document.querySelector('body');
 
     iconoHamburguesa.addEventListener('click', e => {
         let desplegado = iconoHamburguesa.getAttribute('data-desplegado');
@@ -19,10 +24,12 @@ async function crearNavBar() {
         if(desplegado == 'false') {
             linksNavBar.classList.add('abrir');
             iconoHamburguesa.setAttribute('data-desplegado', 'true');
+            body.style.overflow = 'hidden';
         }
         else {
             linksNavBar.classList.remove('abrir');
             iconoHamburguesa.setAttribute('data-desplegado', 'false');
+            body.style.overflow = 'visible';
         }
     });
 
--- entregable-5/js/homepage.js
+++ entregable-5/js/homepage.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', e => {
     crearNavBar().then(e => {
         crearReproductor().then(e => {
             crearCards().then(e => {
-                let flechasIzq = document.querySelectorAll('.flecha__izquierda');
-                let flechasDer = document.querySelectorAll('.flecha__derecha');
+                let flechasIzq = document.querySelectorAll('.contenedor-card__flecha-izquierda');
+                let flechasDer = document.querySelectorAll('.contenedor-card__flecha-derecha');
                 let pixeles = 200;
 
                 flechasIzq.forEach(flecha => {
@@ -31,19 +31,49 @@ document.addEventListener('DOMContentLoaded', e => {
                             contenedorMusica.style.left = (leftActual - pixeles) + 'px';  
                     });
                 });
+
+                let reproductor = document.querySelector('.reproductor');
+                let contenedorCancion = reproductor.children[4];
+                let tituloCancion = contenedorCancion.children[0];
+                let nombreCancion = contenedorCancion.children[1];
+                let iconoReproductor = reproductor.children[2].children[1];
+                console.log(iconoReproductor);
+                
+                //cuando termina la animación que cambia el titulo
+                //saco la clase de la animación
+                contenedorCancion.addEventListener('animationend', e => {
+                    contenedorCancion.classList.remove('cambiarTitulo');
+                });
+
+                //cambio el nombre de la canción y aplico una animación
+                //también cambio el ícono de play por pausa
+                document.querySelectorAll('.contenedor-card__card-content-item').forEach(itm => {
+                    itm.addEventListener('click', e => {
+                        let artista = itm.children[1].innerHTML;
+                        let cancion = itm.children[2].innerHTML;
+
+                        tituloCancion.innerHTML = artista;
+                        nombreCancion.innerHTML = cancion;
+
+                        iconoReproductor.classList.remove('fa-play-circle');
+                        iconoReproductor.classList.add('fa-pause-circle');
+
+                        contenedorCancion.classList.add('cambiarTitulo');
+                    });
+                }); 
             });
         })
     });
 });
 
 async function crearCards() {
-    let titles = ['AÚN SIN ESCUCHAR', 'ESCUCHADO RECIENTEMENTE', 'RECOMENDADO PARA VOS', 'LO MÁS ESCUCHADO'];
+    let titles = ['AÚN SIN VALORAR', 'ESCUCHADO RECIENTEMENTE', 'RECOMENDADO PARA VOS', 'LO MÁS ESCUCHADO'];
     let cardMusica = await fetch('templates/card-musica.html');
     cardMusica = await cardMusica.text();
 
     titles.forEach(title => {
         document.querySelector('.main-container').innerHTML += cardMusica;
-        let titulos = document.querySelectorAll('.title');
+        let titulos = document.querySelectorAll('.contenedor-card__card-title');
         titulos[titulos.length-1].innerHTML = title;
     });
 
