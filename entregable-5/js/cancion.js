10c413f820ddacf7e1462a99c899f96874fbe71a
mrivasalfano <mrivasalfano@gmail.com>
2020-07-20 20:17:01 -0300

    diferenciacion entre banda y artista home

--- entregable-5/js/cancion.js
+++ entregable-5/js/cancion.js
@@ -22,8 +22,8 @@ document.addEventListener('DOMContentLoaded', e => {
         estrella.addEventListener('mouseleave', e => {
             for (let i = 4; i >= 0; i--) {
                 if(estrellas[i].getAttribute('seleccionada') == 'false') {
-                    estrellas[i].classList.remove('fa-star', 'bordeAmarillo');                
-                    estrellas[i].classList.add('fa-star-o');
+                    estrellas[i].classList.remove('fas', 'bordeAmarillo');                
+                    estrellas[i].classList.add('far');
                 }
                 else
                     break;
@@ -34,15 +34,15 @@ document.addEventListener('DOMContentLoaded', e => {
             let nroEstrella = estrella.getAttribute('data-valoracion');
 
             for (let i=4; i>=nroEstrella; i--) {
-                estrellas[i].classList.remove('fa-star', 'bordeAmarillo');                
-                estrellas[i].classList.add('fa-star-o');
+                estrellas[i].classList.remove('fas', 'bordeAmarillo');                
+                estrellas[i].classList.add('far');
                 estrellas[i].setAttribute('seleccionada', 'false');          
             }
 
             for (let i = 0; i < nroEstrella; i++) {
-                estrellas[i].classList.remove('fa-star-o', 'animacionSeleccion', 'bordeAmarillo'); 
+                estrellas[i].classList.remove('far', 'animacionSeleccion', 'bordeAmarillo'); 
                 void estrellas[i].offsetWidth; //para poder volver a aplicar la animación             
-                estrellas[i].classList.add('fa-star', 'bordeAmarillo', 'animacionSeleccion');      
+                estrellas[i].classList.add('fas', 'bordeAmarillo', 'animacionSeleccion');      
                 estrellas[i].setAttribute('seleccionada', 'true');          
             }
 
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', e => {
                     <div class="opiniones__contenido-item-titulo">Usuario logeado dice:</div>
                     <div class="opiniones__contenido-item-mensaje">${opinion.value}</div>
                     <div class="opiniones__contenido-item-valoracion">
-                        <i class="fa fa-star"></i>
+                        <i class="fa fas"></i>
                         <div>${valoracionActual}</div>
                     </div>
                 </div>`;
