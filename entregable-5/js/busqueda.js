873ed50756bb28c56ab70463ce2420ee6580ee5b
mrivasalfano <mrivasalfano@gmail.com>
2020-07-30 19:10:30 -0300

    fix show on scroll

--- entregable-5/js/busqueda.js
+++ entregable-5/js/busqueda.js
@@ -90,11 +90,8 @@ function scrollAnimation() {
 
     function loop() {
     elementsToShow.forEach(function (element) {
-        if (isElementInViewport(element)) {
+        if (isElementInViewport(element))
             element.classList.add('is-visible');
-        } else {
-            element.classList.remove('is-visible');
-        }
     });
 
     scroll(loop);
